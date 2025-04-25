232015bed9390d9a2bf6f53cbc24705583848fb7
Mohamed Imran <54463675+mohamedimmu@users.noreply.github.com>
2025-04-26 02:28:11 +0530

    css update

--- src/components/DropSection.tsx
+++ src/components/DropSection.tsx
@@ -70,7 +70,7 @@ const DropSection = () => {
                   quality={95}
                   // priority={index < 4}
                 />
-                <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+                <div className="absolute inset-0 bg-gradient-to-t from-white/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 ease-in-out" />
               </div>
             </div>
           ))}
