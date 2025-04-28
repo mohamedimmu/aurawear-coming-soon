6fffda299fa03ac424a77f7c773ce36bb1a922c2
Mohamed Imran <54463675+mohamedimmu@users.noreply.github.com>
2025-04-29 00:33:25 +0530

    server env error fix check

--- src/env/server.ts
+++ src/env/server.ts
@@ -1,13 +1,23 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
+// export const envServer = createEnv({
+//   server: {
+//     SITE_ID: z.string().min(1),
+//     API_KEY: z.string().min(1),
+//     ACCOUNT_ID: z.string().min(1),
+//     MEASUREMENT_ID: z.string().min(1),
+//     VERCEL_URL: z.string().optional(),
+//   },
+//   experimental__runtimeEnv: process.env,
+// });
+
 export const envServer = createEnv({
   server: {
     SITE_ID: z.string().min(1),
     API_KEY: z.string().min(1),
     ACCOUNT_ID: z.string().min(1),
     MEASUREMENT_ID: z.string().min(1),
-    VERCEL_URL: z.string().optional(),
   },
-  experimental__runtimeEnv: process.env
+  experimental__runtimeEnv: process.env,
 });
