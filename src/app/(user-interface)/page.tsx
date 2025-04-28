1529045edc05a44cbf04a265ff59c70aa79e380f
Mohamed Imran <54463675+mohamedimmu@users.noreply.github.com>
2025-04-29 00:18:59 +0530

    testing envserver added

--- src/app/(user-interface)/page.tsx
+++ src/app/(user-interface)/page.tsx
@@ -2,12 +2,14 @@ import DropSection from "@/components/DropSection";
 import ContactForm from "@/components/ContactForm";
 import HeroSection from "@/components/HeroSection";
 import { Metadata } from "next";
+import { envServer } from "@/env/server";
 
 export const metadata: Metadata = {
   title: "Coming Soon",
 };
 
 export default function RootLayout() {
+  console.log(envServer.VERCEL_URL, envServer.SITE_ID);
   return (
     <div className="space-y-8 md:space-y-12">
       <HeroSection />
