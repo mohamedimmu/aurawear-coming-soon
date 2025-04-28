e3d614b22473a68ff94fdd4556a02a6270768d7c
Mohamed Imran <54463675+mohamedimmu@users.noreply.github.com>
2025-04-29 00:15:42 +0530

    server env file added

--- src/app/(user-interface)/page.tsx
+++ src/app/(user-interface)/page.tsx
@@ -2,8 +2,6 @@ import DropSection from "@/components/DropSection";
 import ContactForm from "@/components/ContactForm";
 import HeroSection from "@/components/HeroSection";
 import { Metadata } from "next";
-// import { envServer } from "@/env/server";
-// import { envClient } from "@/env/client";
 
 export const metadata: Metadata = {
   title: "Coming Soon",
